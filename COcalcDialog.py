93d6596224d3a958d8eafb856f56e3be8627d3f0
Kent Ogden <ogdenk@upstate.edu>
2016-08-15 16:03:51 -0400

    8/10/16 (steph)

--- COcalcDialog.py
+++ COcalcDialog.py
@@ -1,13 +1,23 @@
 from PyQt4.QtGui import *
 from PyQt4 import QtCore, QtGui
-import QtWidgets
+#import QWidget
 import ui_COcalc
 import ctypes
 import PatientData
+
+from matplotlib import pyplot as plt
+from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as Canvas
+from matplotlib.figure import Figure
+#from matplotlib import rcParams
+
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_qt4agg import (
+    FigureCanvasQTAgg as FigureCanvas)
+
 import numpy as np
 import sys
 
-class myTableWidget(QtGui.QTableWidget):
+"""class myTableWidget(QtGui.QTableWidget):
 
    def keyPressEvent(self,event):
        if(event.key() == QtCore.Qt.Key_Enter):
@@ -19,16 +29,17 @@ class myTableWidget(QtGui.QTableWidget):
 #2) from within Designer, promote your existing QTableWidgets to the new class. All promoted widgets will use
 #the subclass's event code, and will still have their properties set correctly as specified by Designer.
 #http://stackoverflow.com/questions/5747304/event-catch-only-qt-key-delete
-
+"""
 class COcalcDialog(QDialog, ui_COcalc.Ui_CO_Calculator):
     def __init__(self, parent=None):
         super(COcalcDialog, self).__init__(parent)
         self.setupUi(self)
-        self.timeInterval.setPlainText("2") #this will always be 2? or will user possibly change it?
+        self.timeInterval.setPlainText("2") #want to allow user the option to change this value
         self.HUtoIodineConversion.setPlainText("24")
         self.buttonBox.button(QtGui.QDialogButtonBox.Apply).clicked.connect(self.Apply)
         self.buttonBox.button(QtGui.QDialogButtonBox.Reset).clicked.connect(self.Reset)
         self.patient = PatientData.Patient # this is the object that holds the data and does the calculations
+        """
         self.HUvalues = myTableWidget(CO_Calculator)
         self.HUvalues.setGeometry(QtCore.QRect(40, 20, 141, 571))
         self.HUvalues.setMouseTracking(True)
@@ -40,39 +51,68 @@ class COcalcDialog(QDialog, ui_COcalc.Ui_CO_Calculator):
         self.HUvalues.verticalHeader().setVisible(True)
         self.HUvalues.verticalHeader().setCascadingSectionResizes(False)
         self.HUvalues.verticalHeader().setSortIndicatorShown(False)
+        """
         self.Reset
 
     def Apply(self, parent=None):
-        a = []
+        a = ([])
         allRows = self.HUvalues.rowCount()
         for i in np.arange(0, allRows+1, 1):
             temp = self.HUvalues.item(i,0)
             if (temp):
-                a.append(eval(str(temp.text())))
-        a = np.array(a)
-        #ctypes.windll.user32.MessageBoxA(0, "Hello!", "test", 1)
-
+                a.append([eval(str(temp.text()))])
+        a = np.array(a) #type int32
 
         self.patient.data = a
-        self.patient.offset = b #error: b not defined. offset = t0
-        self.patient.data = a - b #if offset is a value, do we need to make 'b' an array of the same size as 'a' to subtract?
-        self.patient.getCoeffs(0)
-        self.patient.getR2()
-        self.patient.tpeak = self.patient.alpha * self.patient.beta
-        self.patient.getContData()
-        self.patient.calcCO()
+        #self.patient.offset = b #error: b not defined. offset = t0
+        #self.patient.data = a - b #if offset is a value, do we need to make 'b' an array of the same size as 'a' to subtract?
+        #self.patient.getCoeffs(0)
+        #self.patient.getR2()
+        #self.patient.tpeak = self.patient.alpha * self.patient.beta
+        #self.patient.getContData()
+        #self.patient.calcCO()
 
-        self.ui.widget.canvas.ax.clear()
-        self.ui.widget.canvas.ax.plot(#self.patient.contTimes, self.patient.contData, self.patient.times, self.patient.data, 'bs')
-        self.ui.widget.canvas.draw()
+        #this doesnt work:
+        fig = Figure()
+        self.plotwidget.canvas = FigureCanvas(fig)
+        self.plotwidget.emptyfig = {}
+
+        self.plotwidget.axes.clear()
+        self.plotwidget.axes.autoscale(enable = True, axis = 'both', tight = None)
+
+        #print(self.patient.data.dtype) type int32
+        #s = self.patient.data.size #type int
+        #print(s)
+        t = self.timeInterval.toPlainText() #type str
+        #print(t)
+        x = np.arange(0, 100, int(t), np.dtype(np.int32)) #makes x-axis in terms of entered time intervals.(int type i/p)
+        # have to turn all items into int first (is int ok? if not, linspace is better than arange)
+        xvalues = ([])
+        includedx = self.patient.data.size
+        for j in np.arange(0, includedx+1, 1):
+            temp2 = x.item(j)
+            if(temp2):
+                xvalues.append(temp2)
+        xvalues = np.array(xvalues)
+        #print(xvalues)
+
+        line1 = self.plotwidget.axes.plot(xvalues, self.patient.data)
+
+        self.plotwidget.axes.legend((line1), ['Patient data'])
+
+        self.plotwidget.axes.set_title([''])
+        self.plotwidget.axes.set_xlabel(['Time (s)'])
+        self.plotwidget.axes.set_ylabel(['Concentration (Cmg)/I'])
+        self.plotwidget.draw()
+
+        #self.ui.plotwidget.canvas.ax.plot(self.patient.contTimes, self.patient.contData, self.patient.times, self.patient.data, 'bs')
+        #self.ui.widget.canvas.draw()
         #self.ui.lineEdit_24.setText("%.7s" % str(self.patient.CO))
         #self.ui.lineEdit_23.setText("%.7s" % str(self.patient.AUC))
         #self.ui.lineEdit_25.setText("%.7s" % str(self.patient.R2))
-
         #print(self.patient.CO)
 
     def Reset(self, parent=None):
-        self.plotwidget.axes.clear()#ax gives error. changed to axes. reset works ok now
         self.HUvalues.clear()
         self.alpha.clear()
         self.beta.clear()
@@ -99,6 +139,12 @@ class COcalcDialog(QDialog, ui_COcalc.Ui_CO_Calculator):
         self.patient.contData = 0
         self.patient.AUC = 0
         self.patient.CO = 0
+
+        #self.plotwidget.axes.clear()
+        self.plotwidget.emptyfig = self.plotwidget.fig
+
+
+
 
 
 
